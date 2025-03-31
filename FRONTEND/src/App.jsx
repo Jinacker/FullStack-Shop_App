1da5d9d063622cfbefcf920d4b46f548518b3385
Jinacker <rlawls1448@naver.com>
2025-03-31 23:27:58 +0900

    Router 셋팅

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,18 +1,40 @@
-/* eslint-disable no-unused-vars */
-import { useState } from 'react'
+
+import { Outlet, Route, Routes } from 'react-router-dom'
 import './App.css'
 
+// Pages import
+import LandingPage from "./pages/LandingPage";
+import LoginPage  from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+// 메인 페이지 컴포넌트들 import
+import Navbar from "./layout/Navbar";
+import Footer from "./layout/Footer";
+
+function Layout() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <main>
+        <Outlet></Outlet>
+      </main>
+      <Footer></Footer>
+    </div>
+  )
+} 
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return ( // tailwind css를 써보자 ~ ! 이렇게 css 없이 바로바로 넣어서 만들수있다!
-    <>
-      <div className = "App">
-        <h1 className='text-3xl font-bold underline'>
-          Hello world !
-        </h1>
-      </div>
-    </>
+    <Routes>
+      <Route path ='/' element = {<Layout></Layout>} >
+        <Route index element = {<LandingPage></LandingPage>}></Route>
+        
+        <Route path = '/login' element ={<LoginPage></LoginPage>}></Route>
+        <Route path = '/register' element ={<RegisterPage></RegisterPage>}></Route>
+        
+      </Route>
+    </Routes>
   )
 }
 
