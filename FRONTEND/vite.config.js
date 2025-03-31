4a4cf3f68feb6e35cf7490feb1eab933aae441dc
Jinacker <rlawls1448@naver.com>
2025-03-31 15:58:31 +0900

    eslint 설정 (오류 밑줄)

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslint from "vite-plugin-eslint" // eslint 설정 적용
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
 })
+
+
+//npm install -D eslint@^9 eslint-plugin-react-hooks eslint-plugin-react-refresh globals @eslint/js
+// eslint 설치. => 내꺼 강의 보다 최신버전이라 좀 다름..
