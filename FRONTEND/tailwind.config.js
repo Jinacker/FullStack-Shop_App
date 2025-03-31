982070ae32b8934a70543a62f9e2b4561cf406ee
Jinacker <rlawls1448@naver.com>
2025-03-31 22:26:58 +0900

    tailwind css 셋팅

---
+++ FRONTEND/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// tailwind css 사용법 #######
+
+// npm install -D tailwindcss@3.4.17 postcss autoprefixer => 이렇게 설치
+// npx tailwindcss init -p  => 이거 실행
+
