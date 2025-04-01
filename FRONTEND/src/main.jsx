4f1a05e0e83d85a3fba62f464e6ac2d88aae1af1
Jinacker <rlawls1448@naver.com>
2025-04-01 11:48:50 +0900

    Redux-Persist

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -3,12 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { store } from './store'
+import { persistor, store } from './store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from 'react-redux';
+
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Provider store ={store} >
+      <PersistGate loading = {null} persistor={persistor}>
       <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 )
