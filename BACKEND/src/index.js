9c5d057a561e9c25fc2fb0b546cd6b19767fad89
Jinacker <rlawls1448@naver.com>
2025-04-11 23:32:27 +0900

    feat: express.json 미들웨어 추가 date:2025/4/11 1.request를 받기 위한 json을 읽을수있는 미들웨어 추가

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -4,21 +4,38 @@
 const express = require('express');
 
 const path = require('path');
+
 const cors = require('cors')
 const app = express();
+
 const port = 4001;
 
-app.get('/',(req,res) =>{
-    res.send('안녕하세요');
-})
-
-//////////미들웨어 사용 app.use 사용
+//////////미들웨어 등록 app.use 사용
 
 // static => 정적인 파일 제공 가능해짐
 app.use(express.static(path.join(__dirname,'../uploads'))); // 이러면 이제 uploads 폴더에 있는 파일들을 클라이언트에게 전달 가능.
 
 //cors => origin이 다르면 서로 통신 못함 (4000 != 3000) => Cross- Origin Resource Sharing (CORS) 사용해서 교차 출처 자원 공유를 해야함.
-app. use(cors());
+app.use(cors());
+
+//express.json => JSON 쓸수있게됨. 파싱 가능
+app.use(express.json());
+
+
+///////////////////
+
+
+// HTTP GET 메서드
+app.get('/',(req,res) =>{
+    res.send('안녕하세요');
+})
+
+// HTTP POST 메서드
+app.post('/',(req,res)=> {
+    console.log(req.body);
+    res.json(req.body);
+})
+
 
 
 
